2ea29ce1622d8e13dd0a8b165abaa45938551ef3
Zach Freedman <zfreedman92@gmail.com>
2018-08-09 14:56:42 -0400

    feat: Ran first successful full game loop
    Family member input working properly
    Abstracted instruction_strings.py outputs into single dictionary
    Added methods to game.py to allow for hat/picker generation AND order validity checking (does the hat process have to be restarted)

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
-from instruction_strings import INPUT_INFO, INPUT_MEMBER_0, INPUT_MEMBER_1, INPUT_MEMBER_2, INPUT_STOP, QUIT, WELCOME
+from instruction_strings import instructions
 from family_printer import family_printer
 from player import Player
+from random import shuffle
 
 # game states
 STATE_INPUT = "input"
@@ -12,12 +13,22 @@ class Game:
     def __init__(self):
         self.family = {}
         self.input_done_str = "done"
+        self.max_iterations = 100
         self.quit_str = "quit"
         self.start_str = "run"
         self.state = STATE_START
 
     def add_member(self, name, partner_name, gift):
         self.family[name] = Player(name, partner_name, gift)
+
+    def check_game_order_validity(self, picker_order, hat_order):
+        for i in range(len(picker_order)):
+
+            if (picker_order[i] == hat_order[i]
+                or self.family[picker_order[i]].partner_name == hat_order[i]):
+
+                return False
+        return True
 
     def game_loop(self):
         while self.state != STATE_OVER:
@@ -28,40 +39,61 @@ class Game:
                 self.handle_input()
 
             elif self.state == STATE_PROCESS:
-                self.state = STATE_OVER
+                self.handle_process()
 
         self.handle_quit()
 
+    def get_random_family_order(self):
+        names = list(self.family.keys())
+        shuffle(names)
+        return names
+
     def handle_input(self):
-        print(INPUT_INFO.format(self.input_done_str))
+        print(instructions["INPUT_INFO"].format(self.input_done_str))
         self.input_loop()
 
+    def handle_process(self):
+        print(instructions["PROCESS_INFO"])
+
+        orders_are_valid = False
+        count = 0
+        while not orders_are_valid and count < self.max_iterations:
+            picker_order = self.get_random_family_order()
+            hat_order = self.get_random_family_order()
+            orders_are_valid = self.check_game_order_validity(picker_order, hat_order)
+            print(count)
+            count += 1
+            print(instructions["PROCESS_LIST_DISPLAY"].format(picker_order, hat_order))
+
+        self.state = STATE_OVER
+
     def handle_start(self):
-        welcome_input = input(WELCOME.format(self.start_str)).lower()
+        welcome_input = input(instructions["WELCOME"].format(self.start_str)).lower()
         self.state = STATE_INPUT if welcome_input == self.start_str else STATE_OVER
 
     def handle_quit(self):
-        print(QUIT)
+        print(instructions["QUIT"])
 
     def input_loop(self):
         while self.state == STATE_INPUT:
+            print()
             family_printer(self.family)
 
-            new_name = input(INPUT_MEMBER_0)
-            new_partner_name = input(INPUT_MEMBER_1)
-            new_gift = input(INPUT_MEMBER_2)
-            print()
+            new_name = input(instructions["INPUT_MEMBER_0"])
+            new_partner_name = input(instructions["INPUT_MEMBER_1"])
+            new_gift = input(instructions["INPUT_MEMBER_2"])
 
             self.add_member(new_name, new_partner_name, new_gift)
 
             # check to stop input
-            if input(INPUT_STOP.format(self.input_done_str)).lower() == self.input_done_str:
+            if input(
+                instructions["INPUT_STOP"].format(self.input_done_str)
+            ).lower() == self.input_done_str:
                 self.state = STATE_PROCESS
 
     def print_unique_family_members(self):
         names = self.family.keys()
         partner_map = {}
-
 
         for name in names:
             if (name not in partner_map and
--- instruction_strings.py
+++ instruction_strings.py
@@ -1,18 +1,40 @@
 player_name = "Mr. Raccoon"
 
-INPUT_INFO = """\nGreat! Here's how input worksself.
+INPUT_INFO = """\nGreat! Here's how family member input works.
 We'll prompt you for a family member's name, their partner's name, and the member's gift.
-If the family member's name already corresponds to another member's partner, we'll skip that step.
-We'll continue to do this until you tell us you're done, which you can indicate by typing "{}".\n"""
+We'll continue to do this until you tell us you're done, which you can indicate by typing "{}"."""
 
-INPUT_MEMBER_0 = """Name: """
+INPUT_MEMBER_0 = """\nName: """
 INPUT_MEMBER_1 = """Partner Name (hit ENTER for no partner): """
 INPUT_MEMBER_2 = """Gift: """
 
-INPUT_STOP = """Type "{}" if you are done inputting family members: """
+INPUT_STOP = """\nType "{}" if you are done inputting family members: """
 
-QUIT = """Thanks for playing {}\n""".format(player_name)
+PROCESS_INFO = """\nFantastic! All family members have been entered.
+What we'll do now is randomize the order of the family members. Then, to simulate
+"random picking", we'll randomize a second list of the same family members. The
+first list represents WHO is picking FROM the hat, whereas the second list
+represents WHICH name the WHO will pickself.
+
+New lists will be generated until two lists are returned which correspond to NO
+family members picking their or their partner's name."""
+
+PROCESS_LIST_DISPLAY = """Pick Order: {}\nHat Order: {}\n"""
+
+QUIT = """\nThanks for playing {}\n""".format(player_name)
 
 WELCOME = """Welcome {}.
 Would you like to run the application?
 To run, type "{}". The application will quit otherwise: """.format(player_name, "{}")
+
+instructions = {
+    "INPUT_INFO": INPUT_INFO,
+    "INPUT_MEMBER_0": INPUT_MEMBER_0,
+    "INPUT_MEMBER_1": INPUT_MEMBER_1,
+    "INPUT_MEMBER_2": INPUT_MEMBER_2,
+    "INPUT_STOP": INPUT_STOP,
+    "PROCESS_INFO": PROCESS_INFO,
+    "PROCESS_LIST_DISPLAY": PROCESS_LIST_DISPLAY,
+    "QUIT": QUIT,
+    "WELCOME": WELCOME
+}
